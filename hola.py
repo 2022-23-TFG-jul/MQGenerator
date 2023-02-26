d4724864f429c3a6930a0fec191b89329cc99117
xhm1001 <xhm1001@alu.ubu.es>
2023-02-26 18:57:48 +0100

    Primeros pasos con Flask
    Creación inicial de la aplicación web

---
+++ hola.py
@@ -0,0 +1,13 @@
+# Creacion del flask
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    title = "Generador automatico de problemas"
+    user = {'nombre': 'Alvaro'}
+
+    return render_template('index.html', title=title, user=user)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
