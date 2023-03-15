518ebc6556bc777b73332303bb0ff545f65619b6
xhm1001 <xhm1001@alu.ubu.es>
2023-03-15 13:58:07 +0100

    Mejora codigo registro
    Añadido más líneas de código del apartado Registro

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def index():
 
     return render_template('index.html', title=title, user=user)
 
+@app.route('/registro', methods = ["GET", "POST"])
+def registro():
+    return render_template('registro.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
