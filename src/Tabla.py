320562f5d3651580f2587501513118a09217e4e0
xhm1001 <xhm1001@alu.ubu.es>
2023-03-29 22:32:16 +0200

    Preguntas tipo Tabla terminado en python
    Se han completado los métodos que faltaban para dar solución a todas las cuestiones que hay en las preguntas tipo tabla y comentado el código.

--- src/Tabla.py
+++ src/Tabla.py
@@ -1,9 +1,10 @@
- # pip install pandas
-
 import random
 import string
-# import pandas as pd
+import pandas as pd
+from termcolor import colored
 
+# Creación de pregunta 0 tipo tabla
+# Autor: Álvaro Hoyuelos Martín
 
 class Tabla:
     def __init__(self):
@@ -44,27 +45,83 @@ class Tabla:
                 # El resto de las filas no tienen valor en la cuarta columna
                 completado = ""
             fila.append(completado)
-
-            # Agrego la fila a la tabla
+            
+            # Agrega la fila a la tabla
             self.tabla.append(fila)
 
     def imprimir_tabla(self):
+        # Añade al porcentaje completado el simbolo %
+        for columna in self.tabla:
+            if columna[3] != "" and columna[3] != "%Completado":                
+                columna[3] = str(columna[3]) + "%"
+
         # Calcula el ancho máximo de cada columna
         anchos = [max(len(str(fila[i])) for fila in self.tabla) for i in range(len(self.tabla[0]))]
         
         # Imprime la tabla con las columnas centradas
-        for fila in self.tabla:
-            print("| " + " | ".join(str(elem).center(anchos[i]) for i, elem in enumerate(fila)) + " |")
-
+        for columna in self.tabla:
+            # Si la tarea está al 100% se pone en negrita
+            if columna[3] == "100%":
+                print(colored("| " + " | ".join(str(elem).center(anchos[i]) for i, elem in enumerate(columna)) + " |", attrs=['bold']))
+            else:
+                print("| " + " | ".join(str(elem).center(anchos[i]) for i, elem in enumerate(columna)) + " |")
+               
     def valor_planificado(self):
         PV = 0
-        for row in self.tabla:
-            # Si la tarea esta al 100% completado se suma su CPTP
-            if row[3] == 100:
-                PV += row[1]
+        for columna in self.tabla:
+            # Si la tarea esta al 100% se suma su CPTP
+            if columna[3] == 100:
+                PV += columna[1]
         return PV
+    
+    def coste_actual(self):
+        AC = 0
+        for columna in self.tabla:
+            # Verifica si tiene CRTR la tarea
+            if isinstance(columna[2], int):                
+                # Convierte el valor en un entero
+                AC += int(columna[2])
+        return AC
 
+    def valor_ganado(self):
+        EV = 0
+        for columna in self.tabla:
+            # Verifica si la tarea está empezada
+             if isinstance(columna[3], int):
+                # Multiplica el CPTP con su porcentaje completado
+                EV += columna[1] * columna [3] / 100                 
+        return EV
+    
+    def CPI(self):
+        EV = self.valor_ganado()
+        AC = self.coste_actual()
+        # Verifica si AC es 0, para que no de error
+        if AC == 0:
+            return 0
+        # Se redondea a dos decimales
+        cpi = round(EV / AC, 2)
+        return cpi
+
+    def SPI(self):
+        EV = self.valor_ganado()
+        PV = self.valor_planificado()
+        # Verifica si PV es 0, para que no de error
+        if PV == 0:
+            return 0
+        # Se redondea a dos decimales
+        spi = round(EV / PV, 2)
+        return spi
+    
+# SALIDA POR PANTALLA
 tabla = Tabla()
+valor_planificado = str(tabla.valor_planificado())
+coste_actual = str(tabla.coste_actual()) 
+valor_ganado = str(tabla.valor_ganado())
+cpi = str(tabla.CPI())
+spi = str(tabla.SPI())
 tabla.imprimir_tabla()
-print("El valor planificado (PV) en el momento actual "+ str(tabla.valor_planificado()))
-
+print("El valor planificado (PV) en el momento actual "+ valor_planificado)
+print("El coste actual (AC) en el momento actual "+ coste_actual)
+print("El valor ganado (EV) en el momento actual "+ valor_ganado)
+print("El CPI del proyecto (con dos decimales) "+ cpi)
+print("El SPI del proyecto (con dos decimales) "+ spi)
